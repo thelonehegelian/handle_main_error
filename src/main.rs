2279ca835c7bff1a1a01c4622dfc849f1c446f3a
umair iftikhar <umair.iftikhar@pulsar-systems.fi>
2023-01-12 02:56:28 +0000

    Add read uptime function and read values

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,33 @@
 use error_chain::error_chain;
 
 use std::fs::File;
+
 use std::io::Read;
-fn main() {
-    println!("Hello, world!");
+
+error_chain! {
+    foreign_links {
+        Io(std::io::Error);
+        ParseInt(::std::num::ParseIntError);
+    }
+}
+
+fn read_uptime() -> Result<u64> {
+    let mut uptime = String::new();
+    File::open("/proc/uptime")
+        .and_then(|mut f| f.read_to_string(&mut uptime))
+        .unwrap();
+
+    Ok(uptime
+        .split('.')
+        .next()
+        .ok_or("Could not read uptime")?
+        .parse()?)
+}
+
+fn main() -> Result<()> {
+    match read_uptime() {
+        Ok(uptime) => println!("Uptime: {} seconds", uptime),
+        Err(e) => println!("Error: {}", e),
+    }
+    Ok(())
 }
